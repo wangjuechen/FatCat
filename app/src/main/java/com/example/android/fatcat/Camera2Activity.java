380307d7c26597ee3dbeb6773b1e98841881dcd2
Juechen <wjc.zhw@gmail.com>
2017-05-27 16:39:08 +1200

    CameraFunctionalityFinalVersionFinished

--- app/src/main/java/com/example/android/fatcat/Camera2Activity.java
+++ app/src/main/java/com/example/android/fatcat/Camera2Activity.java
@@ -42,6 +42,29 @@ public class Camera2Activity extends Activity {
     }
 
     @Override
+    protected void onResume() {
+        iv=(ImageView)findViewById(R.id.ImageView);
+        BtnPlay=(Button)findViewById(R.id.BtnPlay);
+        BtnPlay.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                try
+                {
+                    Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 0);
+                }
+                catch(Exception e)
+                {
+                    Log.e("Exception", e.getMessage());
+                }
+            }
+        });
+        super.onResume();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         if(requestCode==0)
@@ -55,14 +78,6 @@ public class Camera2Activity extends Activity {
         {
             Toast.makeText(this, "You didn't take photo", Toast.LENGTH_SHORT).show();
         }
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        return true;
     }
 
 }
